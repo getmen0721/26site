9786948d47e7d26b68fb129b65b817f9de3b864f
getmen0721 <sasaki@two-six.jp>
2016-12-22 15:40:26 +0900

    中村さんに修正してもらった。（主にjs） コメント付きなので、見返せる。

--- assets/javascripts/mainscript.js
+++ assets/javascripts/mainscript.js
@@ -1,79 +1,61 @@
 $(document).ready(function() {
+    //まずは利用する変数を宣言する
+
     /*  スクローラー読み込み */
-
+    // sだけよりも名前付けたほうが分かりやすくて良い
     var s = skrollr.init();
+    //　トップへ戻るやつ
+    var showFlag = false;
+    // jqueryで取得した者は頭に$とかしておくと良い
+    //普通の変数との見分けが楽
+    var $topBtn = $('#page-top');
+    var showFlag = false;
+    var corporateflag = true;
+    var app1flag = true;
+    var contactflag = true;
+    var app2flag = true;
+    var app3flag = true;
+    var addressflag = true;
+    $topBtn.css('bottom', '-100px');
+  // var はまとめれるので、
+  // var a = 10, b = 20, c = 30;
 
+    // jello animated というクラスを使い続けるなら変数に入れておくと便利
+    var classJelloAnimated = "jello animated";
 
     /* ヘッダーのホバー時にアニメーション */
     $(".navbar-brand").hover(
         function() {
-            $(this).toggleClass("jello animated");
+            $(this).toggleClass(classJelloAnimated);
         }
     );
-
-    /* ヘッダーのスクロールアニメーション*/
-    $('.topBtn').on("click", function() {
-        s.setScrollTop(0)
-    });
-    $('#corporate').on("click", function() {
-        s.setScrollTop(200)
-    });
-    $('#service').on("click", function() {
-        s.setScrollTop(700)
-    });
-    $('#address').on("click", function() {
-        s.setScrollTop(1300)
-    });
-    $("#contact").on("click", function() {
-        s.setScrollTop(1900)
-    });
-
-
+    // scrollは何度も呼ばず、一度の呼び出しの中に全て含める
     $(window).scroll(function() {
         var val = $(this).scrollTop();
-        if (val >= 100) {
+        var sTop = $(window).scrollTop();
 
+        if (val >= 100) {
             $(".navbar-brand").removeClass("jello");
         }
 
-    });
-
-    //　トップへ戻るやつ
-    var showFlag = false;
-    var topBtn = $('#page-top');
-    topBtn.css('bottom', '-100px');
-    var showFlag = false;
-    //スクロールが100に達したらボタン表示
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
+        //スクロールが100に達したらボタン表示
+        if (val > 100) {
             if (showFlag == false) {
                 showFlag = true;
-                topBtn.stop().animate({
+                $topBtn.stop().animate({
                     'bottom': '-10px'
                 }, 200);
             }
         } else {
             if (showFlag) {
                 showFlag = false;
-                topBtn.stop().animate({
+                $topBtn.stop().animate({
                     'bottom': '-100px'
                 }, 200);
             }
         }
-    });
 
-    //スクロールしてトップ
-    topBtn.click(function() {
-        s.setScrollTop(0);
-        return false;
-    });
-
-
-    /*corporateの表示アニメーション処理*/
-    var corporateflag = true;
-    $(window).scroll(function() {
-        var sTop = $(window).scrollTop();
-
+        /*corporateの表示アニメーション処理*/
         if (100 <= sTop) {
             if (300 >= sTop) {
                 if (corporateflag) {
@@ -87,20 +69,13 @@ $(document).ready(function() {
             $(".corporateMain").removeClass("rotateInDownLeft animated");
         }
 
-    });
-
-
-    /*serviceの表示アニメーション処理*/
-    var app1flag = true;
-    $(window).scroll(function() {
-        var sTop = $(window).scrollTop();
-
+        /*serviceの表示アニメーション処理*/
         if (700 <= sTop) {
 
             if (900 >= sTop) {
                 if (app1flag) {
                     app1flag = false;
-                    $(".serviceSabu1").toggleClass("jello animated");
+                    $(".serviceSabu1").toggleClass(classJelloAnimated);
                     $("#appicon").attr({
                         src: "assets/images/appicon.gif"
                     });
@@ -109,21 +84,15 @@ $(document).ready(function() {
 
         } else {
             app1flag = true;
-            $(".serviceSabu1").removeClass("jello animated");
+            $(".serviceSabu1").removeClass(classJelloAnimated);
         }
-
-    });
-
-    var app2flag = true;
-    $(window).scroll(function() {
-        var sTop = $(window).scrollTop();
 
         if (700 <= sTop) {
 
             if (900 >= sTop) {
                 if (app2flag) {
                     app2flag = false;
-                    $(".serviceSabu2").toggleClass("jello animated");
+                    $(".serviceSabu2").toggleClass(classJelloAnimated);
                     $("#webicon").attr({
                         src: "assets/images/webicon.gif"
                     });
@@ -132,21 +101,16 @@ $(document).ready(function() {
 
         } else {
             app2flag = true;
-            $(".serviceSabu2").removeClass("jello animated");
+            $(".serviceSabu2").removeClass(classJelloAnimated);
         }
 
-    });
-
-    var app3flag = true;
-    $(window).scroll(function() {
-        var sTop = $(window).scrollTop();
 
         if (700 <= sTop) {
 
             if (900 >= sTop) {
                 if (app3flag) {
                     app3flag = false;
-                    $(".serviceSabu3").toggleClass("jello animated");
+                    $(".serviceSabu3").toggleClass(classJelloAnimated);
                     $("#directionicon").attr({
                         src: "assets/images/directionicon.gif"
                     });
@@ -155,16 +119,10 @@ $(document).ready(function() {
 
         } else {
             app3flag = true;
-            $(".serviceSabu3").removeClass("jello animated");
+            $(".serviceSabu3").removeClass(classJelloAnimated);
         }
 
-    });
-
-    /*addressの表示アニメーション処理*/
-    var addressflag = true;
-    $(window).scroll(function() {
-        var sTop = $(window).scrollTop();
-
+        /*addressの表示アニメーション処理*/
         if (1300 <= sTop) {
             if (1500 >= sTop) {
                 if (addressflag) {
@@ -183,13 +141,8 @@ $(document).ready(function() {
             addressflag = true;
             $("#addressanime").removeClass("rubberBand animated");
         }
-    });
 
-    /* contactの表示アニメーション処理 */
-    var contactflag = true;
-    $(window).scroll(function() {
-        var sTop = $(window).scrollTop();
-
+        /* contactの表示アニメーション処理 */
         if (1900 <= sTop) {
             if (2000 >= sTop) {
                 if (contactflag) {
@@ -207,22 +160,49 @@ $(document).ready(function() {
     });
 
 
+    // click の処理はまとめる
+    /* ヘッダーのスクロールアニメーション*/
+    $('.topBtn').on("click", function() {
+        s.setScrollTop(0)
+    });
+    $('#corporate').on("click", function() {
+        s.setScrollTop(200)
+    });
+    $('#service').on("click", function() {
+        s.setScrollTop(700)
+    });
+    $('#address').on("click", function() {
+        s.setScrollTop(1300)
+    });
+    $("#contact").on("click", function() {
+        s.setScrollTop(1900)
+    });    
+    
+    //スクロールしてトップ
+    $topBtn.click(function() {
+        s.setScrollTop(0);
+        return false;
+    });
     //モーダル起動
-    //モーダルウィンドウを出現させるクリックイベント
-    $("#modal-open").click(function() {
-
-        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
+    // モーダルの中で頻繁に出てくる部分は関数をつくる
+    // この場合は 
+    var modalInit = function(){
         $(this).blur(); //ボタンからフォーカスを外す
         if ($("#modal-overlay")[0]) return false; //新しくモーダルウィンドウを起動しない (防止策1)
-        //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
-
         //オーバーレイを出現させる
         $("body").append('<div id="modal-overlay"></div>');
         $("#modal-overlay").fadeIn("slow");
 
         //コンテンツをセンタリングする
         centeringModalSyncer();
+        return true
+    }
+    //モーダルウィンドウを出現させるクリックイベント
+    $("#modal-open").click(function() {
 
+       if (modalInit() == false){
+         return false;
+       }
         //コンテンツをフェードインする
         $("#modal-content").fadeIn("slow");
 
@@ -388,8 +368,5 @@ $(document).ready(function() {
         });
 
     }
-
-
-
 
 });
